0d24ed2e3d4b54e4b4e8d13ca9d4a6b4273972ad
Michael Quigley <michael@quigley.com>
2021-09-23 22:55:40 -0400

    The beginnings of model parsing (#4)

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/michaelquigley/pfxlog v0.6.1
+	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/yuin/goldmark v1.4.1
 )
--- model.go
+++ model.go
@@ -1,5 +1,12 @@
 package capsule
 
+import (
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"io/fs"
+	"os"
+)
+
 type Model struct {
 	SrcPath string
 	Root    *Node
@@ -26,5 +33,19 @@ type Property struct {
 // Parse a source path into a Model.
 //
 func Parse(srcPath string) (*Model, error) {
+	srcFs := os.DirFS(srcPath)
+	if err := fs.WalkDir(srcFs, ".", parseVisitor); err != nil {
+		return nil, errors.Wrap(err, "parse error")
+	}
 	return nil, nil
+}
+
+func parseVisitor(path string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	if !d.IsDir() {
+		logrus.Infof("'%v'", path)
+	}
+	return nil
 }
