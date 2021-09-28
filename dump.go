bbd93512e8ecb8f56b4e932e2ef268fb2ff32c4d
Michael Quigley <michael@quigley.com>
2021-09-28 16:19:28 -0400

    Dump model (#8)

--- dump.go
+++ dump.go
@@ -1,6 +1,8 @@
 package capsule
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Dump the logical contents of a Model for debugging and inspection.
 //
@@ -10,5 +12,30 @@ func Dump(model *Model) string {
 	}
 	out := fmt.Sprintf("Model:\n")
 	out += fmt.Sprintf("\tCapsule\n\t\tVersion: '%v'\n", model.Capsule.Version)
+	out += "\n"
+
+	out += dumpNode(0, model.Root)
+
+	return out
+}
+
+func dumpNode(i int, node *Node) string {
+	space := level(i)
+	out := fmt.Sprintf("%s[%s]:\n", space, node.Path)
+	for _, p := range node.Properties {
+		out += fmt.Sprintf("%s\t%v (%v)\n", space, p.Name, p.Type)
+	}
+	out += "\n"
+	for _, child := range node.Children {
+		out += dumpNode(i + 1, child)
+	}
+	return out
+}
+
+func level(i int) string {
+	out := ""
+	for j := 0; j < i; j++ {
+		out += "\t"
+	}
 	return out
 }
