ca3f109a640ce827a4b9c9b27d0279eaad97e5fe
Michael Quigley <michael@quigley.com>
2022-01-09 22:17:19 -0500

    full node path strings in dump

--- dump.go
+++ dump.go
@@ -1,19 +1,29 @@
 package capsule
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+)
 
 func (self *Model) Dump() string {
-	return self.dumpNode(self.Root)
+	return self.dumpNode("", self.Root)
 }
 
-func (self *Model) dumpNode(node *Node) string {
-	out := fmt.Sprintf("%v:\n", node.Path)
+func (self *Model) dumpNode(parentPath string, node *Node) string {
+	var nodePath string
+	if parentPath != "" {
+		nodePath = filepath.ToSlash(filepath.Join(parentPath, node.Path))
+	} else {
+		nodePath = node.Path
+	}
+
+	out := fmt.Sprintf("%v:\n", nodePath)
 	for _, feature := range node.Features {
 		out += fmt.Sprintf("+-- %v %v\n", feature.Name, feature.Attributes.String())
 	}
 	out += "\n"
 	for _, child := range node.Children {
-		out += self.dumpNode(child)
+		out += self.dumpNode(nodePath, child)
 	}
 	return out
 }
