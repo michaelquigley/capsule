3905f3063152ae2de53e1eb9e31a3a599841f499
Michael Quigley <michael@quigley.com>
2022-02-10 21:41:25 -0500

    refinements; polish

--- config.go
+++ config.go
@@ -10,17 +10,7 @@ type Config struct {
 	AttributeHandlers []AttributeHandler
 }
 
-type AttributeHandler func(string, fs.DirEntry) map[string]interface{}
-
-func (cfg *Config) GetAttributes(path string, de fs.DirEntry) map[string]interface{} {
-	merged := make(map[string]interface{})
-	for _, handler := range cfg.AttributeHandlers {
-		for k, v := range handler(path, de) {
-			merged[k] = v
-		}
-	}
-	return merged
-}
+type AttributeHandler func(string, fs.DirEntry) Attributes
 
 func DefaultConfig() *Config {
 	return &Config{
@@ -31,22 +21,32 @@ func DefaultConfig() *Config {
 	}
 }
 
-func filenameRole(path string, _ fs.DirEntry) map[string]interface{} {
+func (cfg *Config) GetAttributes(path string, de fs.DirEntry) Attributes {
+	merged := make(Attributes)
+	for _, handler := range cfg.AttributeHandlers {
+		for k, v := range handler(path, de) {
+			merged[k] = v
+		}
+	}
+	return merged
+}
+
+func filenameRole(path string, _ fs.DirEntry) Attributes {
 	base := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
 	if base == "story" {
-		return map[string]interface{}{"role": "story"}
+		return Attributes{"role": "story"}
 	}
 	return nil
 }
 
-func filenameClassType(path string, _ fs.DirEntry) map[string]interface{} {
+func filenameClassType(path string, _ fs.DirEntry) Attributes {
 	switch strings.ToLower(filepath.Ext(path)) {
 	case ".md":
-		return map[string]interface{}{"class": "document", "type": "markdown"}
+		return Attributes{"class": "document", "type": "markdown"}
 	case ".png":
-		return map[string]interface{}{"class": "image", "type": "png"}
+		return Attributes{"class": "image", "type": "png"}
 	case ".jpg":
-		return map[string]interface{}{"class": "image", "type": "jpeg"}
+		return Attributes{"class": "image", "type": "jpeg"}
 	default:
 		return nil
 	}
