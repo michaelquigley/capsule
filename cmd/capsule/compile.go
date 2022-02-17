33e961731568c94414e9b3b8b34efd1c78801045
Michael Quigley <michael@quigley.com>
2022-02-17 16:18:42 -0500

    simpler (#2)

--- cmd/capsule/compile.go
+++ cmd/capsule/compile.go
@@ -3,70 +3,48 @@ package main
 import (
 	"fmt"
 	"github.com/michaelquigley/capsule"
-	"github.com/michaelquigley/capsule/compiler"
+	"github.com/michaelquigley/capsule/compiler/static"
 	_ "github.com/michaelquigley/capsule/compiler/static"
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"strings"
 )
 
 func init() {
-	compileCmd.Flags().StringVarP(&capsulePath, "capsule", "c", ".", "capsule path")
-	compileCmd.Flags().StringVarP(&compilerId, "compiler", "C", "static", "compiler id")
-	compileCmd.Flags().StringVarP(&compilerVars, "compiler-vars", "V", "", "compiler variables <var>=<val>[,<var>=<val>]")
-	rootCmd.AddCommand(compileCmd)
+	rootCmd.AddCommand(newCompileCommand().cmd)
 }
 
-var compileCmd = &cobra.Command{
-	Use:     "compile",
-	Short:   "compile a capsule into a deployable form",
-	Aliases: []string{"cc"},
-	Args:    cobra.ExactArgs(0),
-	Run:     compile,
+type compileCommand struct {
+	cmd          *cobra.Command
+	capsulePath  string
+	resourcePath string
+	buildPath    string
 }
-var capsulePath string
-var compilerId string
-var compilerVars string
 
-func compile(_ *cobra.Command, _ []string) {
-	mdl, err := capsule.Parse(capsulePath, capsule.DefaultConfig())
+func newCompileCommand() *compileCommand {
+	cc := &compileCommand{
+		cmd: &cobra.Command{
+			Use:     "compile",
+			Short:   "compile a capsule into deployable build",
+			Aliases: []string{"cc"},
+			Args:    cobra.ExactArgs(0),
+		},
+	}
+	cc.cmd.Run = cc.run
+	cc.cmd.Flags().StringVarP(&cc.capsulePath, "capsule", "c", "src/", "capsule path")
+	cc.cmd.Flags().StringVarP(&cc.resourcePath, "resource", "r", "resources/", "resource path")
+	cc.cmd.Flags().StringVarP(&cc.buildPath, "build", "b", "build/", "build path")
+	return cc
+}
+
+func (cc *compileCommand) run(_ *cobra.Command, _ []string) {
+	m, err := capsule.Parse(cc.capsulePath, capsule.DefaultConfig())
 	if err != nil {
-		logrus.Fatal(err)
+		panic(err)
 	}
 
-	fmt.Println(mdl.Dump())
+	fmt.Println(m.Dump())
 
-	if ccFactory, err := compiler.Get(compilerId); err == nil {
-		if ccCfg, err := parseCompilerCfg(); err == nil {
-			if cc, err := ccFactory.New(ccCfg); err == nil {
-				if err := cc.Compile(mdl); err != nil {
-					logrus.Fatal(err)
-				}
-			} else {
-				logrus.Fatalf("error creating compiler (%v)", err)
-			}
-		} else {
-			logrus.Fatalf("error parsing compiler config (%v)", err)
-		}
-	} else {
-		logrus.Fatalf("error getting compiler factory (%v)", err)
+	st := static.New(&static.Config{BuildPath: cc.buildPath, ResourcePath: cc.resourcePath})
+	if err := st.Compile(m); err != nil {
+		panic(err)
 	}
-}
-
-func parseCompilerCfg() (compiler.Config, error) {
-	cfg := make(compiler.Config)
-	if compilerVars != "" {
-		variables := strings.Split(compilerVars, ",")
-		for _, variable := range variables {
-			sides := strings.Split(strings.TrimSpace(variable), "=")
-			if len(sides) != 2 {
-				return nil, errors.Errorf("invalid variable def '%v'", variable)
-			}
-			lside := strings.TrimSpace(sides[0])
-			rside := strings.TrimSpace(sides[1])
-			cfg[lside] = rside
-		}
-	}
-	return cfg, nil
 }
--- compiler/static/static.go
+++ compiler/static/static.go
@@ -2,46 +2,21 @@ package static
 
 import (
 	"github.com/michaelquigley/capsule"
-	"github.com/michaelquigley/capsule/compiler"
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 )
 
-type staticCompiler struct {
-	build     string
-	resources string
+type Config struct {
+	BuildPath    string
+	ResourcePath string
 }
 
-func (c *staticCompiler) Compile(m *capsule.Model) error {
-	logrus.Infof("build = '%v'", c.build)
-	logrus.Infof("resources = '%v'", c.resources)
+type compiler struct {
+	cfg *Config
+}
+
+func New(cfg *Config) *compiler {
+	return &compiler{cfg: cfg}
+}
+
+func (c *compiler) Compile(m *capsule.Model) error {
 	return nil
-}
-
-type staticFactory struct{}
-
-func (f *staticFactory) New(cfg compiler.Config) (compiler.Compiler, error) {
-	sc := &staticCompiler{
-		build:     "build/",
-		resources: "resources/",
-	}
-	if v, found := cfg["build"]; found {
-		if s, ok := v.(string); ok {
-			sc.build = s
-		} else {
-			return nil, errors.Errorf("invalid 'build' value")
-		}
-	}
-	if v, found := cfg["resources"]; found {
-		if s, ok := v.(string); ok {
-			sc.resources = s
-		} else {
-			return nil, errors.Errorf("invalid 'resources' value")
-		}
-	}
-	return sc, nil
-}
-
-func init() {
-	compiler.Register("static", &staticFactory{})
 }
