de62188b2c43fb75aaaf7a75042af7a40276a606
Michael Quigley <michael@quigley.com>
2022-03-30 22:37:31 -0400

    better capsule.Features testing (#26)

--- feature_test.go
+++ feature_test.go
@@ -5,66 +5,54 @@ import (
 	"testing"
 )
 
-func Test_Node_FeaturesWith(t *testing.T) {
+func Test_Note_FeaturesApi(t *testing.T) {
 	n := &Node{}
 	n.Features = append(n.Features, &Feature{Name: "1", Attributes: Attributes{"a": "A", "b": "B"}})
 	n.Features = append(n.Features, &Feature{Name: "2", Attributes: Attributes{"1": "10", "2": "20", "3": 30}})
 	n.Features = append(n.Features, &Feature{Name: "3", Attributes: Attributes{"a": "A", "4": 40}})
 
-	ftrs := n.Features.With(Attributes{"a": "A"})
-	assert.Equal(t, 2, len(ftrs))
-	assert.Equal(t, "1", ftrs[0].Name)
-	assert.Equal(t, "3", ftrs[1].Name)
+	t.Run("with", func(t *testing.T) {
+		ftrs := n.Features.With(Attributes{"a": "A"})
+		assert.Equal(t, 2, len(ftrs))
+		assert.Equal(t, "1", ftrs[0].Name)
+		assert.Equal(t, "3", ftrs[1].Name)
 
-	ftrs = n.Features.With(Attributes{"not": "found"})
-	assert.Equal(t, 0, len(ftrs))
+		ftrs = n.Features.With(Attributes{"not": "found"})
+		assert.Equal(t, 0, len(ftrs))
 
-	ftrs = n.Features.With(Attributes{"3": 30})
-	assert.Equal(t, 1, len(ftrs))
-	assert.Equal(t, "2", ftrs[0].Name)
-}
+		ftrs = n.Features.With(Attributes{"3": 30})
+		assert.Equal(t, 1, len(ftrs))
+		assert.Equal(t, "2", ftrs[0].Name)
+	})
 
-func Test_Node_FeaturesWithout(t *testing.T) {
-	n := &Node{}
-	n.Features = append(n.Features, &Feature{Name: "1", Attributes: Attributes{"a": "A", "b": "B"}})
-	n.Features = append(n.Features, &Feature{Name: "2", Attributes: Attributes{"1": "10", "2": "20", "3": 30}})
-	n.Features = append(n.Features, &Feature{Name: "3", Attributes: Attributes{"a": "A", "4": 40}})
+	t.Run("without", func(t *testing.T) {
+		ftrs := n.Features.Without(Attributes{"a": "A"})
+		assert.Equal(t, 1, len(ftrs))
+		assert.Equal(t, "2", ftrs[0].Name)
+	})
 
-	ftrs := n.Features.Without(Attributes{"a": "A"})
-	assert.Equal(t, 1, len(ftrs))
-	assert.Equal(t, "2", ftrs[0].Name)
-}
+	t.Run("with without", func(t *testing.T) {
+		ftrs := n.Features.With(Attributes{"a": "A"}).Without(Attributes{"4": 40})
+		assert.Equal(t, 1, len(ftrs))
+		assert.Equal(t, "1", ftrs[0].Name)
+	})
 
-func Test_Node_FeaturesWithWithout(t *testing.T) {
-	n := &Node{}
-	n.Features = append(n.Features, &Feature{Name: "1", Attributes: Attributes{"a": "A", "b": "B"}})
-	n.Features = append(n.Features, &Feature{Name: "2", Attributes: Attributes{"1": "10", "2": "20", "3": 30}})
-	n.Features = append(n.Features, &Feature{Name: "3", Attributes: Attributes{"a": "A", "4": 40}})
+	t.Run("name in", func(t *testing.T) {
+		ftrs := n.Features.NameIn([]string{"1", "2"})
+		assert.Equal(t, 2, len(ftrs))
+		assert.Equal(t, "1", ftrs[0].Name)
+		assert.Equal(t, "2", ftrs[1].Name)
+	})
 
-	ftrs := n.Features.With(Attributes{"a": "A"}).Without(Attributes{"4": 40})
-	assert.Equal(t, 1, len(ftrs))
-	assert.Equal(t, "1", ftrs[0].Name)
-}
+	t.Run("name not in", func(t *testing.T) {
+		ftrs := n.Features.NameNotIn([]string{"1", "2"})
+		assert.Equal(t, 1, len(ftrs))
+		assert.Equal(t, "3", ftrs[0].Name)
+	})
 
-func Test_Node_NameIn(t *testing.T) {
-	n := &Node{}
-	n.Features = append(n.Features, &Feature{Name: "1", Attributes: Attributes{"a": "A", "b": "B"}})
-	n.Features = append(n.Features, &Feature{Name: "2", Attributes: Attributes{"1": "10", "2": "20", "3": 30}})
-	n.Features = append(n.Features, &Feature{Name: "3", Attributes: Attributes{"a": "A", "4": 40}})
-
-	ftrs := n.Features.NameIn([]string{"1", "2"})
-	assert.Equal(t, 2, len(ftrs))
-	assert.Equal(t, "1", ftrs[0].Name)
-	assert.Equal(t, "2", ftrs[1].Name)
-}
-
-func Test_Node_NameNotIn(t *testing.T) {
-	n := &Node{}
-	n.Features = append(n.Features, &Feature{Name: "1", Attributes: Attributes{"a": "A", "b": "B"}})
-	n.Features = append(n.Features, &Feature{Name: "2", Attributes: Attributes{"1": "10", "2": "20", "3": 30}})
-	n.Features = append(n.Features, &Feature{Name: "3", Attributes: Attributes{"a": "A", "4": 40}})
-
-	ftrs := n.Features.NameNotIn([]string{"1", "2"})
-	assert.Equal(t, 1, len(ftrs))
-	assert.Equal(t, "3", ftrs[0].Name)
+	t.Run("with name in", func(t *testing.T) {
+		ftrs := n.Features.With(Attributes{"a": "A"}).NameIn([]string{"3"})
+		assert.Equal(t, 1, len(ftrs))
+		assert.Equal(t, "3", ftrs[0].Name)
+	})
 }
